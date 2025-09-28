f7966a7bcd99bbb63329eb075983dac8fc13dc96
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-09-28 19:05:02 +0500

    Update Task1.java

--- Laba_1/src/Task1.java
+++ Laba_1/src/Task1.java
@@ -72,7 +72,8 @@ public class Task1 {
         if (x % 10 == 1 && x != 11)
             return x + " Год";
 
-        else if ((x % 10 == 2 || x % 10 == 3 || x % 10 == 4) && (x != 12 && x != 13 && x != 14))
+        else if ((x % 10 == 2 || x % 10 == 3 || x % 10 == 4) &&
+                 (x != 12 && x != 13 && x != 14))
             return x + " Года";
 
         else
@@ -241,4 +242,5 @@ public class Task1 {
         }
         return result;
     }
+
 }
